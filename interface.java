cb4d669f4d01c85681901bbd399821eaa4151c5a
Mark <mark-angus@outlook.com>
2016-09-19 22:53:13 +0800

    Set up project locally

--- interface.java
+++ interface.java
@@ -1,8 +1,10 @@
 //interface for budget software
 
-Public class interface{
-    Public static void main(String[] args){
+public class interface{
+    public static void main(String[] args){
         // allow user to add account 
-       Public void addAccount(String name, Money value){
+       public void addAccount(String name,Money value){
            newAccount = new account();
         }
+    }
+}
