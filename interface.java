96e86b2ee9daed6563c8a55b13677d44599ee213
Mark Angus <mark.angus@outlook.com>
2016-09-19 22:33:09 +0800

    Interface for System

---
+++ interface.java
@@ -0,0 +1,8 @@
+//interface for budget software
+
+Public class interface{
+    Public static void main(String[] args){
+        // allow user to add account 
+       Public void addAccount(String name, Money value){
+           newAccount = new account();
+        }
