7a55678c4e5c5e9ef520e9e6fe0e5a1f5cc0308b
Mark <mark-angus@outlook.com>
2016-09-20 23:25:14 +0800

    Buttons added and GUI displaying

--- InterfacePage.java
+++ InterfacePage.java
@@ -1,38 +1,53 @@
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class InterfacePage {
 	
-	//Main Window Frame
-	JFrame mainWindow = new JFrame("INAGB");
+	public static void main(String args[]){
+		//Main Window Frame
+		JFrame mainWindow = new JFrame("INAGB");
 	
-	int width = 1080;
-	int height = 1920;
+		int width = 1080;
+		int height = 1920;
 	
-	//Button Display Panel
-	JPanel buttonContainer = new JPanel();
-	buttonContainer.setLayout(new flowLayout());
-	container.setpreferredSize(new Dimension(width, height));
+		//Button Display Panel
+		JPanel buttonContainer = new JPanel();
+		buttonContainer.setLayout(new FlowLayout());
+		buttonContainer.setPreferredSize(new Dimension(width, height));
 	
-	//Buttons
-	JButton accountsButton = new JButton("Accounts");
-	accountsButton.setPreferredSize(new Dimension(width/2,height/4));
-	JButton incomeButton = new JButton("Income");
-	JButton expensesButton = new JButton("Expenses");
-	JButton envelopesButton = new JButton("Envelopes");
-	JButton accountTransfersButton = new JButton("Account Transfers");
-	JButton scheduledPaymentsButton = new JButton("Scheduled Payments");
-	JButton notesButton = new JButton("Notes");
+		//Buttons
+		JButton accountsButton = new JButton("Accounts");
+		accountsButton.setPreferredSize(new Dimension(width/2,height/4));
+		JButton incomeButton = new JButton("Income");
+		incomeButton.setPreferredSize(new Dimension(width/2,height/8));
+		JButton expensesButton = new JButton("Expenses");
+		expensesButton.setPreferredSize(new Dimension(width/2,height/8));
+		JButton envelopesButton = new JButton("Envelopes");
+		envelopesButton.setPreferredSize(new Dimension(width,height/4));
+		JButton accountTransfersButton = new JButton("Account Transfers");
+		accountTransfersButton.setPreferredSize(new Dimension(width,height/4));
+		JButton scheduledPaymentsButton = new JButton("Scheduled Payments");
+		scheduledPaymentsButton.setPreferredSize(new Dimension(width/2,height/4));
+		JButton notesButton = new JButton("Notes");
+		notesButton.setPreferredSize(new Dimension(width/2,height/4));
 
-	buttonContainer.add("accountsButton");
-	buttonContainer.add("incomeButton");
-	buttonContainer.add("expensesButton");
-	buttonContainer.add("envelopesButton");
-	buttonContainer.add("accountTransfersButton");
-	buttonContainer.add("scheduledPaymentsButton");
-	buttonContainer.add("notesButton");
-	
-	buttonContainer.pack();
-	
+		buttonContainer.add(accountsButton);
+		buttonContainer.add(incomeButton);
+		buttonContainer.add(expensesButton);
+		buttonContainer.add(envelopesButton);
+		buttonContainer.add(accountTransfersButton);
+		buttonContainer.add(scheduledPaymentsButton);
+		buttonContainer.add(notesButton);
+		
+		mainWindow.add(buttonContainer);
+		
+		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainWindow.pack();
+		mainWindow.setVisible(true);
+	}
+		
 }
--- interface.java
+++ interface.java
@@ -1,10 +1,10 @@
 //interface for budget software
 
-public class interface{
-    public static void main(String[] args){
+public class interface {
+    //public static void main(String[] args){
         // allow user to add account 
        public void addAccount(String name,Money value){
            newAccount = new account();
         }
-    }
+   // }
 }
